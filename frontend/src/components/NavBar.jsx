6273768b1d6f58918703cdd1f45ad81b165407f5
Vivek Patel <vivekpatel2991@gmail.comm>
2026-09-11 13:33:24 +0530

    #2 – Routing Setup and barBar | Full Stack Project Series

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import '../style/navbar.css'
+
+function NavBar(){
+    return(
+        <>
+            <nav className="navbar">
+                <div className="logo">To Do App</div>
+                <ul className="nav-links">
+                    <li><Link to="/">Task List</Link></li>
+                    <li><Link to="/add">Add Task</Link></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
